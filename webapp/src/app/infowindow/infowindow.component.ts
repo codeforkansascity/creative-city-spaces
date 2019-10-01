a5c02e36de725113e3f3c599922f3c0076b8b700
Doug Mitchell <doug.l.mitchell@gmail.com>
2019-09-30 20:00:47 -0500

    bind data object to infowindow

--- webapp/src/app/infowindow/infowindow.component.ts
+++ webapp/src/app/infowindow/infowindow.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-infowindow',
@@ -6,10 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./infowindow.component.scss']
 })
 export class InfowindowComponent implements OnInit {
+  @Input() data: any;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
